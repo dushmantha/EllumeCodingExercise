c75556cb95c7dea69facd5e91d09cc7ef59bbe10
dushmantha <tdmihiran@gmail.com>
2022-04-03 21:03:09 +1200

    Initial Commit

---
+++ EllumeCodingExercise/EllumeCodingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EllumeCodingExerciseApp.swift
+//  EllumeCodingExercise
+//
+//  Created by Tharaka Dushmantha on 3/04/22.
+//
+
+import SwiftUI
+
+@main
+struct EllumeCodingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EllumeCodingExerciseUITests/EllumeCodingExerciseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EllumeCodingExerciseUITestsLaunchTests.swift
+//  EllumeCodingExerciseUITests
+//
+//  Created by Tharaka Dushmantha on 3/04/22.
+//
+
+import XCTest
+
+class EllumeCodingExerciseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
